810e4db94bc51312b6a16b4116da8596481d2555
wizeshou <yuijam@live.com>
2020-11-21 23:17:29 +0900

    add dockerfile

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import * as path from 'path';
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, '..', 'dist/kaieru')));
+app.use('/', express.static(path.join(__dirname, '..', 'client')));
 
 app.listen(3000, () => {
   console.log('listening 3000');
