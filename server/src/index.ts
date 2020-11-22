093e60a1cb89d14282cf36b0ce1e4e6bbcc4e4b9
wizeshou <yuijam@live.com>
2020-11-22 15:35:10 +0900

    add health

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import * as path from 'path';
 const app = express();
 
+app.get('/health', (req, res) => {
+  res.status(200);
+  res.send('ok');
+});
+
 app.use('/', express.static(path.join(__dirname, '..', 'client')));
 
 app.listen(3000, () => {
