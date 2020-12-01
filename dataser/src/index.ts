b0bea106bc20eb8d450b78ee01022a9e635b0f6c
yuijam <yuijam@live.com>
2020-12-01 16:14:44 +0900

    add healthy check

--- dataser/src/index.ts
+++ dataser/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import parseTwitter from './parser';
 import {end} from './db/pool';
+import http from 'http';
 
 const result = dotenv.config({path: path.join(__dirname, '..', '.env')});
 if (result.error) {
@@ -15,3 +16,11 @@ const main = async () => {
 
 const interval = 5 * 60 * 1000;
 setInterval(() => main(), interval);
+
+const healthyCheckSer = http.createServer((req, res) => {
+  res.end('dataser is ok');
+});
+
+healthyCheckSer.listen('3300', () => {
+  console.log('healthyCheckSer is listen on 3300...');
+});
