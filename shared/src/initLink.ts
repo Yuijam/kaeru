1d177f3ceb0d1890f1fec1000b1abdf75e3c0592
yuijam <yuijam@live.com>
2020-12-04 17:30:39 +0900

    add prisma, adjust initLinks

--- shared/src/initLink.ts
+++ shared/src/initLink.ts
@@ -2,10 +2,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const distDir = path.join(__dirname, '..', 'dist');
-if (!fs.existsSync(distDir)) {
-  throw '/dist is not exist';
+const prismaDir = path.join(__dirname, '..', 'prisma');
+if (!fs.existsSync(distDir) || !fs.existsSync(prismaDir)) {
+  throw 'shared/dist or shared/prisma is not exist';
 }
 const targetProjConfig = ['client', 'server', 'dataser'];
+const prismaTargetProjConfig = ['server', 'dataser'];
 
 const targetProj = targetProjConfig.filter(proj => {
   const targetPath = path.join(__dirname, '..', '..', `${proj}/node_modules`);
@@ -14,10 +16,25 @@ const targetProj = targetProjConfig.filter(proj => {
 
 console.log('targetProj', targetProj);
 
+const prismaTargetProj = prismaTargetProjConfig.filter(proj => {
+  const targetPath = path.join(__dirname, '..', '..', `${proj}`);
+  return fs.existsSync(targetPath);
+});
+
+console.log('prismaTargetProj', prismaTargetProj);
+
 targetProj.map(proj => {
   const targetPath = path.join(__dirname, '..', '..', `${proj}/node_modules/shared`);
   if (fs.existsSync(targetPath)) {
     fs.unlinkSync(targetPath);
   }
   fs.symlinkSync(distDir, targetPath);
+});
+
+prismaTargetProj.map(proj => {
+  const targetPath = path.join(__dirname, '..', '..', `${proj}/prisma`);
+  if (fs.existsSync(targetPath)) {
+    fs.unlinkSync(targetPath);
+  }
+  fs.symlinkSync(prismaDir, targetPath);
 });
