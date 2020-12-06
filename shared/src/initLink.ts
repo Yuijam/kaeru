cd1e094799811d8f40fd5cd4b63b6f176415d5d7
yuijam <yuijam@live.com>
2020-12-06 16:00:52 +0900

    sync prism from dataser to server

--- shared/src/initLink.ts
+++ shared/src/initLink.ts
@@ -1,23 +1,51 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import {exec} from 'child_process';
 
 const distDir = path.join(__dirname, '..', 'dist');
 if (!fs.existsSync(distDir)) {
   throw '/dist is not exist';
 }
+const prismaDir = path.join(__dirname, '..', '..', 'dataser/prisma');
+if (!fs.existsSync(prismaDir)) {
+  throw 'prisma dir is not exist';
+}
+
 const targetProjConfig = ['client', 'server', 'dataser'];
+const targetPrismaConfig = ['server'];
 
 const targetProj = targetProjConfig.filter(proj => {
   const targetPath = path.join(__dirname, '..', '..', `${proj}/node_modules`);
   return fs.existsSync(targetPath);
 });
 
+const targetPrismaProj = targetPrismaConfig.filter(proj => {
+  const targetPath = path.join(__dirname, '..', '..', proj);
+  return fs.existsSync(targetPath);
+});
+
 console.log('targetProj', targetProj);
 
+// sync shared file
 targetProj.map(proj => {
   const targetPath = path.join(__dirname, '..', '..', `${proj}/node_modules/shared`);
   if (fs.existsSync(targetPath)) {
     fs.unlinkSync(targetPath);
   }
   fs.symlinkSync(distDir, targetPath);
+});
+
+// sync prisma model
+targetPrismaProj.map(proj => {
+  const targetPrismaPath = path.join(__dirname, '..', '..', `${proj}/prisma`);
+  if (fs.existsSync(targetPrismaPath)) {
+    fs.unlinkSync(targetPrismaPath);
+  }
+  exec(`cp -r ${prismaDir} ${targetPrismaPath}`, err => {
+    if (err) {
+      console.log(`cp -r ${prismaDir} ${targetPrismaPath} failed! ${err}`);
+    } else {
+      console.log(`copy prisma to ${targetPrismaPath} ok!`);
+    }
+  });
 });
