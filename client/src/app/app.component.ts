bf0775d03360109925cec8c7409dcbcdf9c3631f
yuijam <yuijam@live.com>
2020-12-07 12:21:54 +0900

    client connect to local server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { map } from 'rxjs/operators';
+const GET_RECORDS = gql`
+  {
+    records {
+      id
+      msgId
+      createdAt
+    }
+  }
+`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kaieru';
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    console.log('on init');
+    this.apollo
+      .watchQuery({
+        query: GET_RECORDS,
+      })
+      .valueChanges.pipe(map((res) => res))
+      .subscribe(console.log);
+  }
 }
