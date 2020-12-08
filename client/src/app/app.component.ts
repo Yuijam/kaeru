217c3904e7600b0d9ca3ce5fa7f68b0fd02885c9
yuijam <yuijam@live.com>
2020-12-08 11:37:42 +0900

    add material

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { GetRecordsGQL, GetRecordsQuery } from '../generated/graphql-types';
 import { Observable } from 'rxjs';
+import { MatAccordion } from '@angular/material/expansion';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'kaieru';
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+  panelOpenState = false;
 
   records: Observable<GetRecordsQuery['records']>;
   constructor(private getRecordsGQL: GetRecordsGQL) {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { DemoMaterialModule } from './material-module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'fill' },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
