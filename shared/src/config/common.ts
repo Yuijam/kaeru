6b7ec25a563ede4f7f0b266ed2ad6b459f552943
yuijam <yuijam@live.com>
2020-12-11 20:05:15 +0900

    remove shared

--- shared/src/config/common.ts
+++
@@ -1,3 +0,0 @@
-export const startHour = 4;
-export const endHour = 12;
-export const startDate = new Date(2020, 11, 10);
--- shared/src/helper/index.ts
+++
@@ -1,5 +0,0 @@
-export type TEntity = {
-  id: string;
-};
-
-export type TPartialEntity<T> = Partial<Omit<T, 'id'>>;
