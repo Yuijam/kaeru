b75de751980863c0e14a90758532c114414c01f5
yuijam <yuijam@live.com>
2020-12-14 09:48:24 +0900

    safari time fix

--- server/src/resolvers/query.ts
+++ server/src/resolvers/query.ts
@@ -5,7 +5,9 @@ import {getTodayRecords} from '../db/record';
 
 export const Query: QueryResolvers = {
   lineRecords: async (parent, args) => {
+    console.log(args.date);
     if (args.date && !isValid(new Date(args.date))) {
+      console.log('returen;ll');
       return [];
     }
     const queryDate = args.date ? new Date(args.date) : new Date();
