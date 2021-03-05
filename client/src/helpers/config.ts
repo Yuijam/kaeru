7a828438c487ab6fe79750b1af9b9c376bcc4769
yuijam <yuijam@live.com>
2021-03-05 12:27:49 +0900

    add time text

--- client/src/helpers/config.ts
+++ client/src/helpers/config.ts
@@ -1,5 +1,7 @@
 export const startHour = 4;
-export const endHour = 12;
+export const startHourText = `0${startHour}:00`;
+export const endHour = 24;
+export const endHourText = `${endHour}:00`;
 export const startDate = new Date(2020, 11, 11);
 
 export type TLineConfig = {
